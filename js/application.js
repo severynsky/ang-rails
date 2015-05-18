70a3ceabd24816cdb9ba66c9e13551b7e755d7c9
severynsky <severynr@gmail.com>
2015-05-18 09:53:23 +0300

    init commit

---
+++ js/application.js
@@ -0,0 +1,20 @@
+var myApp = angular.module('myApp', []);
+
+myApp.controller('mainController', [
+  '$scope', 
+  function($scope) {
+    $scope.test = "Hello world!",
+    $scope.posts = [
+      {title: 'post 1', upvotes: 5},
+      {title: 'post 2', upvotes: 2},
+      {title: 'post 3', upvotes: 15},
+      {title: 'post 4', upvotes: 9},
+      {title: 'post 5', upvotes: 4}
+    ]
+
+    $scope.addPost = function(){
+      $scope.posts.push({
+        title: $scope.title, upvotes: 0});
+        $scope.title = "";
+    };
+}]);
